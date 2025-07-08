394c2b141981ee4d0a4b59516da2be66798de324
minh le <117999133+sixxson@users.noreply.github.com>
2025-07-08 13:28:31 +0700

    ...

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -13,8 +13,7 @@ const router = express.Router();
 
 // Chỉ người dùng đăng nhập mới truy cập
 router.get("/user/:id", authenticateToken, GetUserById);
-router.patch("/user/:id", authenticateToken, UpdateUser);
-router.put("/user/:id", EditUser);
+router.put("/user/:id", authenticateToken, UpdateUser);
 
 // Chỉ admin mới truy cập
 router.put("/user/:id", authenticateToken, authorizeAdmin, EditUser);
